a6a0bad735982a6b1a1b04357589089481a55b87
JeffersonXimenes <jefferson.ximenes11@hotmail.com>
2019-10-31 10:25:58 -0300

    testando_mesclando

---
+++ acompanhamento_api.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, jsonify, request, render_template
+from resources.acompanhamentoPedido import AcompanhamentoPedido,AcompanhamentoNaoExiste, IdFuncNaoExiste, IdVendaNaoExiste,\
+    DataInvalida,ErroInserir
+from infraestrutura.to_dict import to_dict, to_dict_list
+
+acompanhamento_app = Blueprint('acompanhamento_app', __name__, template_folder='templates')
+
+@acompanhamento_app.route('/acompanhamentoPedido', methods=['GET'])
+def login():
+   return render_template("DiarioDeBordo.html")
+
+@acompanhamento_app.route('/cadastroAcompanhamento', methods=['GET'])
+def listar():
+    listar = AcompanhamentoPedido.get(AcompanhamentoPedido)
+    return jsonify(listar)
+ 
+
+@acompanhamento_app.route('/cadastroAcompanhamento', methods=['POST'])
+def criar():
+    descricao = request.form["iDescricao"]  
+    dataAtualizacao = request.form["dataAtualizacao"]
+    matricula = request.form["cFuncionario"]  
+    pedido = request.form["cPedido"]
+    
+    dados = {"iDescricao": descricao, "cFuncionario": matricula, "cPedido": pedido,"dataAtualizacao": dataAtualizacao}
+
+    try:
+        AcompanhamentoPedido.post(AcompanhamentoPedido) 
+        return 'Acompanhamento cadastrado com sucesso', 201
+    
+    except IdVendaNaoExiste:
+        return "mensagem: Pedido não encontrado.", 500
+    except IdFuncNaoExiste:
+        return "mensagem: Funcionario não encontrado.", 500
+    except ErroInserir:
+        return "mensagem: Ocorreu um erro ao inserir o endereco.", 500
+    except DataInvalida:
+        return "mensagem: Data invalida.", 500
+
