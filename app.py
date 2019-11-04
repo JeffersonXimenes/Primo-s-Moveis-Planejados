af190294c749e5762541856c2b92e4fa6d569f40
JoaoVicctor <42067702+JoaoVicctor@users.noreply.github.com>
2019-11-04 19:35:26 -0300

    feedback atualizado

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from funcionarios_api import funcionarios_app
 from tipoPagamento_api import tipoPagamento_app
 from vendas_api import vendas_app
 from acompanhamento_api import acompanhamento_app
+from feedback_api import feedback_app
 
 app = Flask(__name__)
 app.register_blueprint(acompanhamento_app)
@@ -19,6 +20,7 @@ app.register_blueprint(cargo_app)
 app.register_blueprint(funcionarios_app)
 app.register_blueprint(tipoPagamento_app)
 app.register_blueprint(vendas_app)
+app.register_blueprint(feedback_app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'segredo'
